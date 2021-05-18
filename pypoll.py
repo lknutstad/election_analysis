3e45712c3f2a30449226aff29a2b03951bcb6b8a
lknutstad <lknutstad@gmail.com>
2021-05-17 22:09:19 -0700

    updated README.md with election results summary.

--- pypoll.py
+++ pypoll.py
@@ -72,3 +72,4 @@ with open(file_to_save, "w") as txt_file:
     print(winning_candidate_summary)
     # Save the winning candidate's results to the text file.
     txt_file.write(winning_candidate_summary)
+print(total_votes)
